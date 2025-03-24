6597be05d03dd3ce795c525a051ae3fdde1e666a
SleiYah <shy300602@gmail.com>
2025-03-24 04:26:52 +0200

    pushed the migrations

---
+++ hr-server/database/migrations/2025_03_22_103139_create_employees_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::disableForeignKeyConstraints();
+
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email');
+            $table->string('password_hash');
+            $table->dateTime('date_of_birth');
+            $table->string('address');
+            $table->integer('phone_number');
+            $table->bigInteger('dep_id');
+            $table->foreign('dep_id')->references('id')->on('departments');
+            $table->bigInteger('pos_id');
+            $table->foreign('pos_id')->references('id')->on('positions');
+            $table->enum('gender', ["Male", "Female"]);
+            $table->decimal('salary');
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at');
+            $table->timestamp('deleted_at');
+        });
+
+        Schema::enableForeignKeyConstraints();
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
---
+++ hr-server/database/migrations/2025_03_22_103141_create_performances_reviews_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::disableForeignKeyConstraints();
+
+        Schema::create('performances_reviews', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('emp_id');
+            $table->foreign('emp_id')->references('id')->on('employees')->onDelete('cascade')
+            ->onUpdate('cascade');
+            $table->bigInteger('rev_cyc_id');
+            $table->foreign('rev_cyc_id')->references('id')->on('review_cycles')->onDelete('cascade')
+            ->onUpdate('cascade');
+            $table->bigInteger('overall_rating');
+            $table->string('comments');
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at');
+            $table->timestamp('deleted_at');
+        });
+
+        Schema::enableForeignKeyConstraints();
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('performances_reviews');
+    }
+};
