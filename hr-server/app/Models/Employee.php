fc5207682a8cfc978227523d220a0806d6033487
SleiYah <shy300602@gmail.com>
2025-03-25 02:54:52 +0200

    done with the employee crud statements

--- hr-server/app/Models/Employee.php
+++ hr-server/app/Models/Employee.php
@@ -6,6 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Employee extends Model
 {
+    protected $fillable = [
+        'department_id',
+        'position_id',
+        'first_name',
+        'last_name',
+        'email',
+        'password',
+        'date_of_birth',
+        'address',
+        'phone_number',
+        'gender',
+        'role',
+        'salary'
+    ];
     public function department()
     {
         return $this->belongsTo(Department::class);
