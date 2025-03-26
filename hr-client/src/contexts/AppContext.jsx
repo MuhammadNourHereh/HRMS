7db0a89cd17309992ffbf14e38faa2588e7a8b0d
SleiYah <shy300602@gmail.com>
2025-03-26 15:25:41 +0200

    pushing structure

---
+++ hr-client/src/contexts/AppContext.jsx
@@ -0,0 +1,13 @@
+import { createContext, useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
+export const AppContext = createContext(null)
+
+const navigate = useNavigate()
+return (
+    <AppContext.Provider value={{
+        navigate,
+    
+    }}>
+        {children}
+    </AppContext.Provider>
+)
