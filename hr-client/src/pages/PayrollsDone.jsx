048c18ec1132a1922ff4ade0d1e1736941d5eac4
Muhammadnourhereh <acadamiclearner@gmail.com>
2025-03-27 01:31:50 +0200

    add payrolls done page

--- hr-client/src/pages/PayrollsDone.jsx
+++ hr-client/src/pages/PayrollsDone.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const PayrollsDone = () => {
+    const navigate = useNavigate()
     return (
-        <div className="container margin-left">
-            Payrolls Done
+        <div className='margin-left center container'>
+            <div className="center column">
+                <div className='circle center'>
+                    <img className='fill-image' src="/check.svg" alt="check" />
+                </div>
+                <p className='big-text'>Payrolls Done!</p>
+                <p className='small-text'>Your employees will receive their payments soon</p>
+                <button className='btn-btn'
+                    onClick={() => navigate("/")}>Go to home screen</button>
+            </div>
         </div>
     )
 }
