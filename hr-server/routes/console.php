09bf0c39d3aefeb0fdd33afa7ca0a69793a58c2a
Muhammadnourhereh <acadamiclearner@gmail.com>
2025-03-26 18:08:23 +0200

    add schadule to calc salary

--- hr-server/routes/console.php
+++ hr-server/routes/console.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Models\Employee;
+use App\Models\Salary;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Schedule;
 use Illuminate\Foundation\Console\ClosureCommand;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
@@ -8,3 +12,15 @@ Artisan::command('inspire', function () {
     /** @var ClosureCommand $this */
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::call(function () {
+    Log::channel('daily')->info('Scheduler started');
+    $employees = Employee::all();
+
+    foreach ($employees as $emp) {
+        Salary::create([
+            "employee_id" => $emp->id,
+            "amount" => $emp->salary,
+        ]);
+    }
+})->weekends();
