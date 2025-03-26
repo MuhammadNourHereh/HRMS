e597e398ceb26cf4322a9ebae23a8ae5cc5f052e
Muhammadnourhereh <acadamiclearner@gmail.com>
2025-03-26 15:53:17 +0200

    set up app context

--- hr-client/src/contexts/AppContext.jsx
+++ hr-client/src/contexts/AppContext.jsx
@@ -1,13 +1,21 @@
 import { createContext, useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
+
+
 export const AppContext = createContext(null)
 
-const navigate = useNavigate()
-return (
-    <AppContext.Provider value={{
-        navigate,
-    
-    }}>
-        {children}
-    </AppContext.Provider>
-)
+
+
+
+export function AppProvider({ children }) {
+    const navigate = useNavigate()
+
+    return (
+        <AppContext.Provider value={{
+            navigate,
+
+        }}>
+            {children}
+        </AppContext.Provider>
+    )
+}
