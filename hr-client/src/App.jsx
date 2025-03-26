9a292025ffb09b409d1854e58935c532af3c2ba1
Malak Atieh <malak.atieh12@gmail.com>
2025-03-26 12:25:10 +0200

    apply pagination

--- hr-client/src/App.jsx
+++ hr-client/src/App.jsx
@@ -7,8 +7,6 @@ import LeaveRequests from "./pages/LeaveRequests";
 import Content from './components/Content';
 import GeolocationLogger from './components/GeolocationLogger';
 import DocumentManagement from './components/DocumentManagement'; // Import DocumentManagement component
-import LeaveRequests from './pages/LeaveRequests';
-import LeaveDetails from './components/LeaveDetails';
 // import LeaveDisplay from './components/LeaveDisplay'; // Use PascalCase for consistency
 
 function App() {
@@ -23,7 +21,6 @@ function App() {
           <Route path="/geolocation" element={<GeolocationLogger />} /> {/* Geolocation page route */}
           <Route path="/document-management" element={<DocumentManagement />} /> {/* Document Management route */}
           <Route path="/LeaveRequests" element={<LeaveRequests />} /> {/* LeaveRequests route */}
-          <Route path="/LeaveDetails" element={<LeaveDetails />} /> {/* LeaveDetails route */}
           {/* <Route path="/leaveDisplay" element={<LeaveDisplay />} /> leaveDisplay route */}
         </Routes>
       </div>
