2d12d80b1fae876ea20a6912ee2bae7244238698
Muhammadnourhereh <acadamiclearner@gmail.com>
2025-03-30 19:53:47 +0300

    fix payrolls css

--- hr-client/src/pages/PayrollsApprove.jsx
+++ hr-client/src/pages/PayrollsApprove.jsx
@@ -10,10 +10,11 @@ const PayrollsApprove = () => {
         navigateHome,
         navigateDone
     } = usePayrollsApprove();
-    return (
-        <div className='container center-warpper' >
 
-            <div className="container2 margin-left">
+
+    return (
+        <div className='column container2 spread'>
+            <div className="container margin-left">
                 <table className='table'>
                     <thead>
                         <tr>
@@ -59,8 +60,7 @@ const PayrollsApprove = () => {
                     <button className='btn-btn' onClick={navigateDone}>Approve Payrolls</button>
                 </div>
             </div>
-            </div>
-            
+        </div>
     )
 }
 
