7bb1f7260fd9a6a105f7cf0e6cdce62ed5710c83
Muhammadnourhereh <acadamiclearner@gmail.com>
2025-04-05 11:54:47 +0300

    build

--- hr-client/env.js
+++ hr-client/env.js
@@ -1,3 +1,3 @@
 const version = 'v0.1'
 export const BASE_URL = `http://13.38.12.19:8000/api/${version}/`
-export const BASE_URL_AXIOS = `http://13.38.12.19:8000/api/${version}/hr`
+export const BASE_URL_AXIOS = `http://13.38.12.19:8000/api/${version}/hr` 
